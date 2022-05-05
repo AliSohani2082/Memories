f8f644827c75fdcf3091fad1643113fb946bd0b0
Ali Sohani <a.sohani.shahrood@gmail.com>
2022-05-05 11:58:38 +0430

    added pagination and search

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,36 +1,116 @@
-import React, { useState, useEffect } from 'react'
-import { Container, Grow, Grid } from '@material-ui/core'
-import Form from '../Form/Form'
-import Posts from '../Posts/Posts'
-import useStyles from './styles'
-import { getPosts } from '../../actions/Posts'
-import { useDispatch } from 'react-redux'
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Grow,
+  Grid,
+  Paper,
+  AppBar,
+  TextField,
+  Button,
+} from "@material-ui/core";
+import { useLocation, useNavigate } from "react-router-dom";
+import ChipInput from "material-ui-chip-input";
+import Form from "../Form/Form";
+import Posts from "../Posts/Posts";
+import useStyles from "./styles";
+import { getPosts } from "../../actions/Posts";
+import { useDispatch } from "react-redux";
+import Pagination from "../Pagination/Pagination";
+import App from "../../App";
 
-
-
-const Home = () => {
-    const [currentId, setCurrentId] = useState(null)
-    const dispatch = useDispatch()
-    const classes = useStyles()
-
-    useEffect(() => {
-        dispatch(getPosts())
-    }, [currentId, dispatch])
-
-    return (
-        <Grow in>
-            <Container>
-                <Grid container className={classes.mainContainer} justifyContent='space-between' alignItems='stretch' spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                    <Posts setCurrentId={ setCurrentId } />
-                    </Grid>
-                    <Grid item xs={12} sm={4}>
-                    <Form currentId={ currentId } setCurrentId={ setCurrentId } />
-                    </Grid>
-                </Grid>
-            </Container>
-        </Grow>
-    )
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
 }
 
-export default Home
+const Home = () => {
+  const [currentId, setCurrentId] = useState(null);
+  const dispatch = useDispatch();
+  const query = useQuery();
+  const navigate = useNavigate();
+  const classes = useStyles();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const handleKeyPress = (e) => {
+    if (e.keyCode === 13) {
+      // search post
+    }
+  };
+
+  const handleAdd = (tag) => {
+    if (tags.find((t) => t === tag) !== -1) {
+      setTags([...tags, tag]);
+    }
+  };
+
+  const handleDelete = (tagToDelete) =>
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+
+  useEffect(() => {
+    dispatch(getPosts());
+  }, [currentId, dispatch]);
+
+  const searchPost = () => {
+    if (search.trim()) {
+      //dispatch -> fetch search post
+    }
+  };
+
+  return (
+    <Grow in>
+      <Container maxWidth="xl">
+        <Grid
+          container
+          className={classes.gridContainer}
+          justifyContent="space-between"
+          alignItems="stretch"
+          spacing={3}
+        >
+          <Grid item xs={12} sm={6} md={9}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                name="search"
+                variant="outlined"
+                label="Search Memories"
+                fullWidth
+                value={search}
+                onKeyPress={handleKeyPress}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <ChipInput
+                style={{ margin: "10px 0" }}
+                value={tags}
+                onAdd={handleAdd}
+                onDelete={handleDelete}
+                label="Search Tags"
+                variant="outlined"
+              />
+              <Button
+                onClick={searchPost}
+                className={classes.searchButton}
+                color="primary"
+              >
+                Search
+              </Button>
+            </AppBar>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+            <Paper className={classes.pagination} elevation={6}>
+              <Pagination />
+            </Paper>
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
